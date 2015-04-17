3e186f81854c0ae4cd497b5442a8474367613495
Tim Smith <tim@cozy.co>
2015-04-17 10:23:36 -0700

    Update copyright, fix headers, make sure everything has encoding

--- lib/change.rb
+++ lib/change.rb
@@ -1,4 +1,20 @@
-#!/bin/ruby
+# encoding: UTF-8
+#
+# Author:: Tim Smith (<tim@cozy.co>)
+# Copyright:: Copyright (c) 2014-2015 Tim Smith
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 begin
   require 'json'
@@ -62,7 +78,6 @@ module Comrad
     end
 
     def print
-
     end
 
     # takes an array of files that have changes and returns hash of chef objects and an action to take on the object
